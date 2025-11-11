e43b4d72cfa4114cefafa6fd9150c537b6c8cd06
marin <marin.peric.tg@gmail.com>
2025-11-11 17:47:04 +0100

    main

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ from database import createDatabase
 
 app = FastAPI(
     title="Freelancer Invoice/Income Tracking System",
-    description="An API for managing freelancer's invoices and incomes",
+    description="An API for managing freelancers' invoices and incomes",
     version="1.0.0"
 )
+
+createDatabase()
+
+app.include_router(clients.router, prefix="/clients", tags=["clients"])
+app.include_router(invoices.router, prefix="/invoices", tags=["invoices"])
+app.include_router(users.router, prefix="/users", tags=["users"])
