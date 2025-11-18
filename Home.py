28f05c67495decee765e0346fb1a64d7229508d6
marin <marin.peric.tg@gmail.com>
2025-11-18 18:21:57 +0100

    main page

--- Home.py
+++ Home.py
@@ -1,7 +1,16 @@
 import streamlit as st
+import requests
+import pandas as pd
+from datetime import datetime
+import plotly.express as px
+import plotly.graph_objects as go
+from dotenv import load_dotenv
+import os
+
+BASE_URL = os.getenv("BASE_URL")
 
 st.set_page_config(
-    page_title = "Main page"
+    page_title = "Home"
 )
 
 st.header("Home")
